b2b65e671cf82f03ba9ffda8da49b19320032a62
uneven39 <unwar39@gmail.com>
2018-01-28 22:55:46 +0200

    legend rendering

--- src/stagesScalePlugin.js
+++ src/stagesScalePlugin.js
@@ -40,7 +40,6 @@
 
     var dayLength = 86400000, // day duration in ms
         timeShift = (new Date).getTimezoneOffset() * 60 * 1000, // TimeZone offset in ms
-        stages,
         firstEventDate,
         lastEventDate,
         totalDuration = 0;
@@ -97,79 +96,133 @@
 
         groupEvents: function(period){
 
-            if (period > 0 && period < 5) {
-                for (var h = 0; h < 24; h++) {
-                    var $group = $events.children().filter(function(index, event) {
-                        var date = new Date($(event).data('date'));
-                        return (date.getHours() === h);
-                    });
-                    if ($group.length > 1) {
-                        var step = $ruler.width() / 24,
-                            $wrapper = $('<div class="group-by"></div>');
-                        switch (period) {
-                            case 1:
-                                var offset = ((step * h + step/2) / $ruler.width()) * 100;
-                                $wrapper
-                                    .addClass('hour')
-                                    .css('left', offset + '%')
-                                    .attr('data-count', $group.length);
-                                $group.wrapAll($wrapper);
-                                break;
-                            case 2:
-                                for (var m30 = 29; m30 <= 59; m30 = m30 + 30) {
-                                    var $groupBy30m = $group.filter(function(index, event) {
-                                        var date = new Date($(event).data('date')),
-                                            mins = date.getMinutes();
-                                        return ((mins >= (m30 - 29)) && (mins <= m30));
-                                    });
-                                    if ($groupBy30m.length > 1) {
-                                        var offsetAdd30m = (m30 + 1 - 15) * step / 60,
-                                            offset30m = ((step * h + offsetAdd30m) / $ruler.width()) * 100;
-                                        $wrapper
-                                            .addClass('30min')
-                                            .css('left', offset30m + '%')
-                                            .attr('data-count', $groupBy30m.length);
-                                        $groupBy30m.wrapAll($wrapper);
-                                    }
+
+            for (var h = 0; h < 24; h++) {
+                var $group = $events.children().filter(function(index, event) {
+                    var date = new Date($(event).data('date'));
+                    return (date.getHours() === h);
+                });
+                if ($group.length > 1) {
+                    var step = $ruler.width() / 24,
+                        $wrapper = $('<div class="group-by"></div>');
+                    switch (period) {
+                        case 1:
+                            var offset = ((step * h + step/2) / $ruler.width()) * 100;
+                            $wrapper
+                                .addClass('hour')
+                                .css('left', offset + '%')
+                                .attr('data-count', $group.length);
+                            $group.wrapAll($wrapper);
+                            break;
+                        case 2:
+                            for (var m30 = 29; m30 <= 59; m30 = m30 + 30) {
+                                var $groupBy30m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return ((mins >= (m30 - 29)) && (mins <= m30));
+                                });
+                                if ($groupBy30m.length > 1) {
+                                    var offsetAdd30m = (m30 + 1 - 15) * step / 60,
+                                        offset30m = ((step * h + offsetAdd30m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('30min')
+                                        .css('left', offset30m + '%')
+                                        .attr('data-count', $groupBy30m.length);
+                                    $groupBy30m.wrapAll($wrapper);
                                 }
-                                break;
-                            case 3:
-                                for (var m15 = 14; m15 <= 59; m15 = m15 + 15) {
-                                    var $groupBy15m = $group.filter(function(index, event) {
-                                        var date = new Date($(event).data('date')),
-                                            mins = date.getMinutes();
-                                        return ((mins >= (m15 - 14)) && (mins <= m15));
-                                    });
-                                    if ($groupBy15m.length > 1) {
-                                        var offsetAdd15m = (m15 + 1 - 7.5) * step / 60,
-                                            offset15m = ((step * h + offsetAdd15m) / $ruler.width()) * 100;
-                                        $wrapper
-                                            .addClass('15min')
-                                            .css('left', offset15m + '%')
-                                            .attr('data-count', $groupBy15m.length);
-                                        $groupBy15m.wrapAll($wrapper);
-                                    }
+                            }
+                            break;
+                        case 3:
+                            for (var m20 = 19; m20 <= 59; m20 = m20 + 20) {
+                                var $groupBy20m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return ((mins >= (m20 - 19)) && (mins <= m20));
+                                });
+                                if ($groupBy20m.length > 1) {
+                                    var offsetAdd20m = (m20 + 1 - 10) * step / 60,
+                                        offset20m = ((step * h + offsetAdd20m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('20min')
+                                        .css('left', offset20m + '%')
+                                        .attr('data-count', $groupBy20m.length);
+                                    $groupBy20m.wrapAll($wrapper);
                                 }
-                                break;
-                            case 4:
-                                for (var m5 = 4; m5 <= 59; m5 = m5 + 5) {
-                                    var $groupBy5m = $group.filter(function(index, event) {
-                                        var date = new Date($(event).data('date')),
-                                            mins = date.getMinutes();
-                                        return ((mins >= (m5 - 4)) && (mins <= m5));
-                                    });
-                                    if ($groupBy5m.length > 1) {
-                                        var offsetAdd5m = (m5 + 1 - 3.75) * step / 60,
-                                            offset5m = ((step * h + offsetAdd5m) / $ruler.width()) * 100;
-                                        $wrapper
-                                            .addClass('5min')
-                                            .css('left', offset5m + '%')
-                                            .attr('data-count', $groupBy5m.length);
-                                        $groupBy5m.wrapAll($wrapper);
-                                    }
+                            }
+                            break;
+                        case 4:
+                            for (var m15 = 14; m15 <= 59; m15 = m15 + 15) {
+                                var $groupBy15m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return ((mins >= (m15 - 14)) && (mins <= m15));
+                                });
+                                if ($groupBy15m.length > 1) {
+                                    var offsetAdd15m = (m15 + 1 - 7.5) * step / 60,
+                                        offset15m = ((step * h + offsetAdd15m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('15min')
+                                        .css('left', offset15m + '%')
+                                        .attr('data-count', $groupBy15m.length);
+                                    $groupBy15m.wrapAll($wrapper);
                                 }
-                                break;
-                        }
+                            }
+                            break;
+                        case 5:
+                            for (var m10 = 9; m10 <= 59; m10 = m10 + 10) {
+                                var $groupBy10m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return ((mins >= (m10 - 9)) && (mins <= m10));
+                                });
+                                if ($groupBy10m.length > 1) {
+                                    var offsetAdd10m = (m10 + 1 - 5) * step / 60,
+                                        offset10m = ((step * h + offsetAdd10m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('10min')
+                                        .css('left', offset10m + '%')
+                                        .attr('data-count', $groupBy10m.length);
+                                    $groupBy10m.wrapAll($wrapper);
+                                }
+                            }
+                            break;
+                        case 6:
+                            for (var m5 = 4; m5 <= 59; m5 = m5 + 5) {
+                                var $groupBy5m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return ((mins >= (m5 - 4)) && (mins <= m5));
+                                });
+                                if ($groupBy5m.length > 1) {
+                                    var offsetAdd5m = (m5 + 1 - 2.5) * step / 60,
+                                        offset5m = ((step * h + offsetAdd5m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('5min')
+                                        .css('left', offset5m + '%')
+                                        .attr('data-count', $groupBy5m.length);
+                                    $groupBy5m.wrapAll($wrapper);
+                                }
+                            }
+                            break;
+                        case 7:
+                        case 8:
+                            for (var m = 0; m <= 59; m++) {
+                                var $groupBy1m = $group.filter(function(index, event) {
+                                    var date = new Date($(event).data('date')),
+                                        mins = date.getMinutes();
+                                    return (mins === m);
+                                });
+                                if ($groupBy1m.length > 1) {
+                                    var offsetAdd1m = (m + 1 - 0.5) * step / 60,
+                                        offset1m = ((step * h + offsetAdd1m) / $ruler.width()) * 100;
+                                    $wrapper
+                                        .addClass('1min')
+                                        .css('left', offset1m + '%')
+                                        .attr('data-count', $groupBy1m.length);
+                                    $groupBy1m.wrapAll($wrapper);
+                                }
+                            }
+                            break;
                     }
                 }
             }
@@ -221,6 +274,40 @@
             });
         },
 
+        redrawLegend: function() {
+            var totalHeight = 0,
+                listHeightLimit,
+                listHeight = 0,
+                start = 0,
+                cols = 0,
+                $legend = $pluginContainer.find('.legend'),
+                $events = $legend.children();
+
+            $events.each(function (index, event) {
+                totalHeight += $(event).height();
+            });
+
+            listHeightLimit = totalHeight / 3;
+
+            for (var i = 0; i < $events.length; i ++) {
+                listHeight += $($events[i]).height();
+                if (listHeight === listHeightLimit) {
+                    $events.slice(start, i + 1).wrapAll('<div class="col-3"></div>');
+                    start = i + 1;
+                    listHeight = 0;
+                    cols += 1;
+                } else if (listHeight > listHeightLimit) {
+                    $events.slice(start, i).wrapAll('<div class="col-3"></div>');
+                    start = i;
+                    listHeight = 0;
+                    cols += 1;
+                }
+                if (cols === 2) break;
+            }
+            if (cols < 3)
+                $events.slice(start, $events.length).wrapAll('<div class="col-3"></div>');
+        },
+
         bind: function() {
             $timeLine
                 .on('scroll zoom', function() {
@@ -229,9 +316,9 @@
                         $legend = $pluginContainer.find('.legend'),
                         $legendContent = $('<div></div>');
 
-                    console.log('time-line scrolling');
-                    console.log($timeLine.css('padding-right').slice(0, -2));
-                    console.log('==========================');
+                    // console.log('time-line scrolling');
+                    // console.log($timeLine.css('padding-right').slice(0, -2));
+                    // console.log('==========================');
 
                     $events.children().each(function(index, el) {
                         var leftBorder = $(el).offset().left;
@@ -242,18 +329,20 @@
                             if (el.classList.contains('group-by')) {
                                 // group:
                                 $(el).children().each(function(index, item) {
-                                    console.log('event ' + item.dataset.title + ' in viewport');
+                                    // console.log('event ' + item.dataset.title + ' in viewport');
                                     var $eventData = $('<div class="legend-item"></div>');
                                     $eventData.append('<h4>' + item.dataset.title + '</h4>' +
                                         '<div class="description">' + item.innerText + '</div>');
+                                    console.log(item.dataset.title, $eventData.height());
                                     $legendContent.append($eventData);
                                 });
                             } else if (el.classList.contains('events-item')) {
                                 // single:
-                                console.log('event ' + el.dataset.title + ' in viewport');
+                                // console.log('event ' + el.dataset.title + ' in viewport');
                                 var $eventData = $('<div class="legend-item"></div>');
                                 $eventData.append('<h4>' + el.dataset.title + '</h4>' +
                                     '<div class="description">' + el.innerText + '</div>');
+                                console.log(el.dataset.title, $eventData.height());
                                 $legendContent.append($eventData);
                             }
                         }
@@ -261,7 +350,8 @@
                     });
 
                     $legend.html($legendContent.html());
-                    console.log('==========================');
+                    helpers.redrawLegend();
+                    // console.log('==========================');
                 });
 
             $controls
@@ -335,6 +425,7 @@
             if ((typeof this === 'object') && this.jquery) {
                 // Вызываем плагин как метод jQuery-элемента:
                 $pluginContainer = this;
+                this.addClass('hidden');
 
                 // Сортируем коллекцию событий по датам, получаем массив событий
                 helpers.sortEventNodes($pluginContainer);
@@ -349,7 +440,9 @@
                 $pluginContainer
                     .children()
                     .wrapAll('<div class="time-line"><div class="events"></div></div>');
-                $timeLine = $pluginContainer.find('.time-line');
+                $timeLine = $pluginContainer
+                    .find('.time-line')
+                    .addClass('zoom-1');
                 $events = $timeLine.find('.events');
                 $pluginContainer
                     .prepend($controls)
@@ -358,11 +451,14 @@
                 helpers.redraw(firstEventDate, lastEventDate);
                 helpers.drawRuler('24h');
 
-                helpers.groupEvents(1);
+                // helpers.groupEvents(1);
 
                 helpers.bind();
 
-                $timeLine.trigger('scroll');
+                this.removeClass('hidden');
+
+                $timeLine.trigger('zoom');
+                helpers.groupEvents(1);
 
             } else if (typeof this === 'function') {
                 // Вызываем плагин как глобальный метод jQuery:
